f9dc6af5c926270d8665901ff5773349b06a5e09
Ravi Kumar Nayak <ravi.nayak@ooma.com>
2023-09-17 18:58:41 -0400

    Updated index.tsx to render AppTest, updated App.css to include styles for display of flexbox properties

--- app/javascript/src/index.tsx
+++ app/javascript/src/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { App } from './App';
+// import { App } from './App';
 import { reportWebVitals } from './reportWebVitals';
+import { AppTest } from './AppTest';
 // import { AppTest } from './AppTest';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 // root.render(<AppTest />);
-root.render(<App />);
+root.render(<AppTest />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
