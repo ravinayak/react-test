d96bdf5cd39ebd9272a67ed623c7e83867021578
Ravi Kumar Nayak <ravi.nayak@ooma.com>
2023-10-29 06:41:00 -0400

    Include methods for iterating over array and inserting, updating, removing elements in array; iterating over properties of an object to remove property with specified value, or update property to new value; iterate over objects in array, and update object which has property with specified value to new value, remove object if found with specified value for a property

--- app/javascript/components/ModernReactWithRouterUdemyCourse/Section-6/BookListShowEdit.tsx
+++ app/javascript/components/ModernReactWithRouterUdemyCourse/Section-6/BookListShowEdit.tsx
@@ -25,6 +25,260 @@
 	9. Update the application to implement Step 7 for updating state for array, objects
 	
 */
+
+/*
+
+The code below will perform the following operations:
+
+  1. Update title of book with given id to new title passed as argument to update function
+  2. Swap the updated book with book at order passed as argument
+      =>  Book at order passed as argument to update function will be swapped with book updated with new title at id passed as 
+          argument to function
+
+const books = [
+  {
+    id: 0,
+    order: 0,
+    title: 'Harry Potter',
+  },
+  {
+    id: 1,
+    order: 1,
+    title: 'Black Widow',
+  },
+  {
+    id: 2,
+    order: 2,
+    title: 'Superman',
+  },
+  {
+    id: 3,
+    order: 3,
+    title: 'Spiderman',
+  },
+  {
+    id: 4,
+    order: 4,
+    title: 'The Matrix Revolution',
+  },
+  {
+    id: 5,
+    order: 5,
+    title: 'The Avengers',
+  }
+];
+
+const swapBook = ({title, order, id}) => {
+  if(order < 0 || order > books.length) {
+    console.log('Order is incorrect - less than 0 or greater than length of array');
+    return books;
+  }
+  let smallerSwapOrder = null;
+  let greaterSwapOrder = null;
+  let insertElementAtSmallerSwapOrder = null;
+  let insertElementAtGreaterSwapOrder = null;
+  let bookAtId = null;
+  let bookWithOrder = null;
+  books.forEach((book) => {
+    if(book.id == id) {
+      bookAtId = book;
+      if(book.order < order) {
+        smallerSwapOrder = book.order;
+        greaterSwapOrder = order;
+      } else {
+        smallerSwapOrder = order;
+        greaterSwapOrder = book.order;
+      }
+    }
+    if(book.order == order) {
+      bookWithOrder = book;
+    }
+  });
+  if(bookAtId.order < order) {
+    insertElementAtSmallerSwapOrder = {
+      ...bookWithOrder,
+      order: bookAtId.order,
+    };
+    insertElementAtGreaterSwapOrder = {
+      ...bookAtId,
+      order,
+      title
+    };
+  } else {
+    insertElementAtSmallerSwapOrder = {
+      ...bookAtId,
+      order,
+      title,
+    };
+    insertElementAtGreaterSwapOrder = {
+      ...bookWithOrder,
+      order: bookAtId.order,
+    };
+  }
+  return [
+    ...books.slice(0, smallerSwapOrder),
+    insertElementAtSmallerSwapOrder,
+    ...books.slice(smallerSwapOrder + 1, greaterSwapOrder),
+    insertElementAtGreaterSwapOrder,
+    ...books.slice(greaterSwapOrder + 1),
+  ];
+}
+swapBook({title: 'Spiderman - Order - 3 to 1', order: 1, id: 3});
+
+*/
+
+/*
+
+The code below inserts elements in Array
+  a. Start of Array
+  b. End of Array
+  c. Insert Element at specified index in Array
+  d. Remove Element at specified index in Array
+
+  const fruits = ['apple', 'cherry', 'guava', 'pears', 'orange', 'mango'];
+
+  a. Start of Array
+
+  const insertFruitAtStart = (fruit) => {
+    return [
+      fruit,
+      ...fruits,
+    ];
+  }
+
+  b. End of Array
+
+  const insertFruitAtEnd = (fruit) => {
+    return [
+      ...fruits,
+      fruit,
+    ];
+  };
+
+  c. Insert Fruit at Specified Index
+
+  const insertFruitAtSpecifiedIndex = (fruit, index) => {
+    return [
+      ...fruits.slice(0, index),
+      fruit,
+      ...fruits.slice(index),
+    ];
+  };
+
+  d. Remove fruit at Specified Index
+
+  const removeFruitAtSpecifiedIndex = (index) => {
+    return fruits.filter((fruit, fruitIndex) => fruitIndex != index );
+  };
+
+  Test Methods with following invocations of functions:
+  
+  insertFruitAtStart('new fruit');
+  insertFruitAtEnd('new fruit');
+  insertFruitAtSpecifiedIndex('new fruit', 3);
+  removeFruitAtSpecifiedIndex(2);
+
+*/
+
+/*
+
+The following code updates existing property of an object from oldValue to newValue and removes property if object
+contains property with specified value
+
+
+  const fruitObject = {
+    id: 1,
+    order: 1,
+    smell: 'tasty',
+    name: 'mango'
+  };
+
+  a. Update existing property of an object to a new value
+
+  const updateProperty = (obj, property, newValue) => {
+    return {
+      ...obj,
+      [property]: newValue,
+    };
+  };
+
+  b. Remove existing property of an object
+
+  const removeProperty = (obj, property) => {
+    let updatedObj = {};
+    for(const [key, value] of Object.entries(obj)) {
+      if(key != property) {
+        updatedObj[key] = value;
+      }
+    };
+    return updatedObj;
+  };
+
+  Test methods with following invocations:
+
+  updateProperty(fruitObject, 'smell', 'smell property updated');
+  removeProperty(fruitObject, 'smell');
+*/
+
+/*
+The following code iterates over objects in an Array and udpates object if it has a property with specified value to new value,
+removes object from array if it has a property with specified value
+
+  const houses = [
+    {
+      id: 1,
+      cost: 1000,
+      area: 500,
+      tag: 'small',
+    },
+    {
+      id: 2,
+      cost: 2000,
+      area: 1000,
+      tag: 'medium',
+    },
+    {
+      id: 3,
+      cost: 3000,
+      area: 2000,
+      tag: 'medium large',
+    },
+    {
+      id: 4,
+      cost: 4000,
+      area: 3000,
+      tag: 'large',
+    }
+  ];
+
+  a. If an object exists in array with specified value for a property, update that property of object from oldValue to newValue
+
+  const updateObjectWithSpecifiedProperty = (property, oldValue, newValue) => {
+    return houses.map((house) => {
+      if(house[property] == oldValue) {
+        return {
+          ...house,
+          [property]: newValue,
+        };
+      }
+      return house;
+    });
+  };
+
+  b. If an object exists in array with specified value for a property, remove that object from array
+
+  const removeObjectWithSpecifiedProperty = (property, value) => {
+    return houses.filter((house) => house[property] != value);
+  };
+
+  Test methods with following invocations:
+
+  updateObjectWithSpecifiedProperty('cost', 3000, 5000);
+  updateObjectWithSpecifiedProperty('tag', 'large', 'large udpated');
+  removeObjectWithSpecifiedProperty('area', 9000);
+  
+*/
+
 import React, { useState } from 'react';
 import { IBook } from '@app/types/bookCreate';
 import { BookCreate } from './BookCreate';
