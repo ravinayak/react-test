87c45fb58a29c44522a905d42dbb4fc30b234cc9
Ravi Kumar Nayak <ravi.nayak@ooma.com>
2023-10-29 20:07:28 -0400

    Included webpack in production, development dependencies; fixed eslint errors in server.js file; updated yarn.lock

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
+import { Logger } from '@app/lib/Logger';
+
+const express = require('express');
 const webpack = require('webpack');
 const WebpackDevServer = require('webpack-dev-server');
 const webpackConfig = require('./webpack-dev-server.config.js'); // Import your webpack configuration
-const express = require('express');
+
 const compiler = webpack(webpackConfig);
 
 const app = express();
@@ -9,12 +12,13 @@ const app = express();
 // Serve static assets using Express from a specific folder
 app.use('/public', express.static('public'));
 
-const devServerOptions = Object.assign({}, webpackConfig.devServer, {
+const devServerOptions = {
+  ...webpackConfig.devServer,
   open: true,
-});
+};
 
 const server = new WebpackDevServer(devServerOptions, compiler);
 
 server.start(8080, 'localhost', () => {
-  console.log('Development server is running on http://localhost:8080');
+  Logger.info('Development server is running on http://localhost:8080');
 });
