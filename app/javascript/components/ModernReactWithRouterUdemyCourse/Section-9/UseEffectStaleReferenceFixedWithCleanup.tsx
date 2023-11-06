955ca028e98243cb1ff0de98d1f6635e2275c49d
Ravi Kumar Nayak <ravi.nayak@ooma.com>
2023-11-06 07:44:46 -0500

    Added explanation for why cleanup is not required when we use useCallback hook, and why it is required when we do not use the hook

--- app/javascript/components/ModernReactWithRouterUdemyCourse/Section-9/UseEffectStaleReferenceFixedWithCleanup.tsx
+++ app/javascript/components/ModernReactWithRouterUdemyCourse/Section-9/UseEffectStaleReferenceFixedWithCleanup.tsx
@@ -26,6 +26,19 @@ export function UseEffectStaleReferenceFixedWithCleanup() {
     logger.info('Logging on Console');
   }, []);
 
+  // Function created without using useCallback which implies a new function object will be created for every render
+  // This will cause a new function object to be attached to click event listener on every render, and multiple click
+  // event listeners will accumulate and stack up, causing a single click on document body to execute these multiple
+  // click event listeners to execute and show log output multiple times
+  // For a counter value of 2, 3 such functions will be attached to click event on document body and each will log output
+  // to console
+  //    1st Render => counter = 0 => useEffect runs => New function object created and attached, # of event listeners attached = 1
+  //    2nd Render => counter = 1 => useEffect runs => New function object created and attached, # of event listeners attached = 2
+  //    3rd Render => counter = 2 => useEffect runs => New function object created and attached, # of event listeners attached = 3
+  // const bodyClickConsoleLogListener = () => {
+  //   logger.info('Logging on Console :: ', Math.random());
+  // };
+
   // Attaching a clickEventListener to document body will end up attaching many click event listeners to document body,
   // precisely one on every render of the component. This must be fixed, as at any point of time, a function which
   // performs a desired functionality should be attached only once to any HTML Element
@@ -35,12 +48,25 @@ export function UseEffectStaleReferenceFixedWithCleanup() {
   //
   useEffect(() => {
     // attach a click handler to document body which prints counter on div
+    // New function object is created for "bodyClickListener" which contains a reference to "counter" state, hence, for
+    // every render a new function object for "bodyClickListener" is attached to document body. These multiple event listeners
+    // stack up and accumulate on the event target
+    // When we click on document body, we see multiple logging output on console, one for each "counter" state starting from
+    // initial state
     document.body.addEventListener('click', bodyClickListener);
+
+    // When you attach the same function as a click event listener to the document body twice, the function will only be
+    // executed once when you click the document body. This is because event listeners are added to the event target and
+    // not duplicated when you add them multiple times AS LONG AS THE FUNCTION OBJECT IS SAME. If you create a new function
+    // object and attach it to document body, a new event listener will be attached to document body
+    // Because we are using useCallback hook, same function object is returned ACROSS RENDERS, hence, only 1 event listener
+    // is attached to document body for "bodyClickConsoleLogListener"
     document.body.addEventListener('click', bodyClickConsoleLogListener);
 
     return () => {
       document.body.removeEventListener('click', bodyClickListener);
-      document.body.removeEventListener('click', bodyClickConsoleLogListener);
+      // Not REQUIRED if we are using useCallback
+      // document.body.removeEventListener('click', bodyClickConsoleLogListener);
     };
     // Because useCallback is used, bodyClickConsoleLogListener function should reference the same object as previous render
     // and NO INFINITE LOOP should occur
